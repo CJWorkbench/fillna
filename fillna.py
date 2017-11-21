bc760adadfb33e77286a80e53d018e041282abb4
Jonathan Stray <jonathanstray@gmail.com>
2017-11-21 14:37:15 -0800

    Update to new API

--- fillna.py
+++ fillna.py
@@ -1,30 +1,16 @@
-class Importable:
-    @staticmethod
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def event():
-        pass
-
-    @staticmethod
-    def render(wf_module, table):
-        cols = wf_module.get_param_string('colnames').split(',')
-        cols = [c.strip() for c in cols]
-
-        if cols == [] or cols == ['']:
-            return table
-
-        for c in cols:
-            if not c in table.columns:
-                wf_module.set_error('There is no column named %s' % c)
-                return table
-
-        method = wf_module.get_param_menu_string('method')
-        if method == 'Down':
-            table[cols] = table[cols].fillna(axis = 0, method='ffill')
-        else:
-            table[cols] = table[cols].fillna(axis=0, method='bfill')
-
-        wf_module.set_ready(notify=False)
+def render(table, params):
+    # Get columns as an array
+    cols = params['colnames']
+    if cols is None:
+        return table    
+    cols = [c.strip() for c in cols.split(',')]
+    if cols == [] or cols == ['']:
         return table
+
+    method = params['method']
+    if method == 'Down':
+        table[cols] = table[cols].fillna(axis = 0, method='ffill')
+    else:
+        table[cols] = table[cols].fillna(axis=0, method='bfill')
+
+    return table
